1acc0ce04de6a44808f66f76e747e3193ec69687
Lucas Eler <eler.lucas@gmail.com>
2026-04-25 11:12:11 -0300

    ajustando payload

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb"
-    }
+  serverActions: {
+    bodySizeLimit: "10mb"
   },
   serverExternalPackages: ["pdf-parse", "pdfjs-dist", "@napi-rs/canvas", "tesseract.js", "tesseract.js-core"]
 };
